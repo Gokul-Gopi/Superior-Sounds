75c02aa1c0d357f998fb0227ebf4b8f29f3d7b98
Gokul606 <68158600+Maxx665@users.noreply.github.com>
2021-10-13 00:13:07 +0530

    fix api link

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
-export const Backend = "http://127.0.0.1:5000"
+// export const Backend = "http://127.0.0.1:5000"
 
-// export const Backend = 'https://superior-sounds-backend.herokuapp.com'
+export const Backend = 'https://superior-sounds-backend.herokuapp.com'
