2af4424e41e13db607c1a1142c9d7db041ac0f74
Gokul Gopi <gokulg1412@gmail.com>
2022-09-06 13:46:47 +0530

    feat: order place functionality

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
 import Header from "./components/Nav-Bar/Header";
 import Home from "./components/Home/Home";
-import Cart from './components/Cart/Cart'
-import { useEffect } from 'react'
-import {
-  Routes,
-  Route,
-} from 'react-router-dom';
+import Cart from "./components/Cart/Cart";
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import ProductListing from "./components/ProductListing/ProductListing";
 import Wishlist from "./components/Wishlist/Wishlist";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
 import Login from "./components/Login/Login";
-import Loader from './components/Loader/Loader'
+import Loader from "./components/Loader/Loader";
 import { useProduct } from "./Context/ProductContext";
 import SignUp from "./components/SignUp/SignUp";
 import { useAuth } from "./Context/AuthContext";
-import { defaultHeaderForToken, getUserCart, getUserWishlist } from "./Utils/NetworkCalls";
+import {
+  defaultHeaderForToken,
+  getUserCart,
+  getUserWishlist,
+} from "./Utils/NetworkCalls";
 import Logout from "./components/Logout/Logout";
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-toast.configure()
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import OrderPlaced from "./components/OrderPlaced/OrderPlaced";
+toast.configure();
 
 function App() {
-  const { state, dispatch } = useProduct()
-  const { authState } = useAuth()
-  defaultHeaderForToken(authState.currentUserToken)
+  const { state, dispatch } = useProduct();
+  const { authState } = useAuth();
+  defaultHeaderForToken(authState.currentUserToken);
 
   useEffect(() => {
-    authState.currentUserToken && getUserCart(dispatch) && getUserWishlist(dispatch)
-  }, [authState.currentUserToken])
+    authState.currentUserToken &&
+      getUserCart(dispatch) &&
+      getUserWishlist(dispatch);
+  }, [authState.currentUserToken]);
 
   return (
     <div className="App">
-      {state.loading ?
-
-        <Loader /> :
+      {state.loading ? (
+        <Loader />
+      ) : (
         <>
           <SignUp />
           <Logout />
@@ -41,15 +45,16 @@ function App() {
           <Header />
 
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='wishlist' element={<Wishlist />} />
-            <Route path='cart' element={<Cart />} />
-            <Route path='products' element={<ProductListing />} />
-            <Route path='products/:id' element={<ProductDetails />} />
-            <Route path='login' element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="wishlist" element={<Wishlist />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="products" element={<ProductListing />} />
+            <Route path="products/:id" element={<ProductDetails />} />
+            <Route path="login" element={<Login />} />
+            <Route path="orderPlaced" element={<OrderPlaced />} />
           </Routes>
         </>
-      }
+      )}
     </div>
   );
 }
--- src/Utils/NetworkCalls.js
+++ src/Utils/NetworkCalls.js
@@ -1,151 +1,154 @@
 import axios from "axios";
 import callToastify from "./toast";
-import { Backend } from '../api'
+import { Backend } from "../api";
 
 const networkCall = async (route, method, data) => {
-    switch (method) {
-        case 'GET':
-            try {
-                return await axios({
-                    method: 'get',
-                    url: `${Backend}${route}`,
-                })
-            } catch (err) {
-                console.log(`Error: ${err.message}`);
-            }
+  switch (method) {
+    case "GET":
+      try {
+        return await axios({
+          method: "get",
+          url: `${Backend}${route}`,
+        });
+      } catch (err) {
+        console.log(`Error: ${err.message}`);
+      }
 
-            break;
+      break;
 
-        case 'POST':
-            try {
-                return await axios({
-                    method: 'post',
-                    url: `${Backend}${route}`,
-                    data: data
-                })
-            } catch (err) {
-                return err.response
-            }
+    case "POST":
+      try {
+        return await axios({
+          method: "post",
+          url: `${Backend}${route}`,
+          data: data,
+        });
+      } catch (err) {
+        return err.response;
+      }
 
+    case "PUT":
+      try {
+        return await axios({
+          method: "put",
+          url: `${Backend}${route}`,
+          data: data,
+        });
+      } catch (err) {
+        return err.message;
+      }
 
-        case 'PUT':
-            try {
-                return await axios({
-                    method: 'put',
-                    url: `${Backend}${route}`,
-                    data: data
-                })
-            } catch (err) {
-                return err.message
-            }
+    case "DELETE":
+      try {
+        return await axios({
+          method: "delete",
+          url: `${Backend}${route}`,
+        });
+      } catch (err) {
+        return err.message;
+      }
 
-
-        case 'DELETE':
-            try {
-                return await axios({
-                    method: 'delete',
-                    url: `${Backend}${route}`,
-                })
-            } catch (err) {
-                return err.message;
-            }
-
-        default:
-            break;
-
-    }
-}
+    default:
+      break;
+  }
+};
 
 const defaultHeaderForToken = (token) => {
-    if (token) {
-        return axios.defaults.headers.common["Authorization"] = token
-    }
-}
+  if (token) {
+    return (axios.defaults.headers.common["Authorization"] = token);
+  }
+};
 
 const getUserCart = async (dispatch) => {
-    dispatch({ type: 'SET_LOADING' })
-    try {
-        const { data } = await networkCall('/cart', 'GET')
-        dispatch({ type: 'SET_CART', payload: data.userCart })
-    } catch (err) {
-        console.log(`Error: ${err.message}`);
-    }
-    dispatch({ type: 'SET_LOADING' })
-}
+  dispatch({ type: "SET_LOADING" });
+  try {
+    const { data } = await networkCall("/cart", "GET");
+    dispatch({ type: "SET_CART", payload: data.userCart });
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+  }
+  dispatch({ type: "SET_LOADING" });
+};
 
 const addToCart = async (event, productID, dispatch, isLoggedIn) => {
-    dispatch({ type: 'SET_LOADING' })
-    event.preventDefault()
+  dispatch({ type: "SET_LOADING" });
+  event.preventDefault();
 
-    if (isLoggedIn) {
-        try {
-            const { data } = await networkCall(`/cart/${productID}`, 'POST')
-            if (data.success) {
-                dispatch({ type: 'SET_CART', payload: data.response })
-                callToastify(' 🛒 Added to cart!')
-            } else {
-                callToastify('Already in cart !')
-            }
-        } catch {
-            callToastify('Something went wrong ! Try Again')
-        }
-    } else {
-        callToastify('You are not logged in', 'Error')
+  if (isLoggedIn) {
+    try {
+      const { data } = await networkCall(`/cart/${productID}`, "POST");
+      if (data.success) {
+        dispatch({ type: "SET_CART", payload: data.response });
+        callToastify(" 🛒 Added to cart!");
+      } else {
+        callToastify("Already in cart !");
+      }
+    } catch {
+      callToastify("Something went wrong ! Try Again");
     }
+  } else {
+    callToastify("You are not logged in", "Error");
+  }
 
-    dispatch({ type: 'SET_LOADING' })
-
-}
+  dispatch({ type: "SET_LOADING" });
+};
 
 const modifyCartItemsQty = async (event, type, productID, dispatch) => {
-    dispatch({ type: 'SET_LOADING' })
-    event.preventDefault()
-    const response = await networkCall(`/cart/${productID}`, 'PUT', type)
-    dispatch({ type: 'SET_CART', payload: response.data.userCart })
-    dispatch({ type: 'SET_LOADING' })
-}
+  dispatch({ type: "SET_LOADING" });
+  event.preventDefault();
+  const response = await networkCall(`/cart/${productID}`, "PUT", type);
+  dispatch({ type: "SET_CART", payload: response.data.userCart });
+  dispatch({ type: "SET_LOADING" });
+};
 
 const removeItemFromCart = async (event, productID, dispatch) => {
-    dispatch({ type: 'SET_LOADING' })
-    event.preventDefault()
-    const { data } = await networkCall(`/cart/${productID}`, 'DELETE')
-    dispatch({ type: 'SET_CART', payload: data.userCart })
-    dispatch({ type: 'SET_LOADING' })
-}
+  dispatch({ type: "SET_LOADING" });
+  event.preventDefault();
+  const { data } = await networkCall(`/cart/${productID}`, "DELETE");
+  dispatch({ type: "SET_CART", payload: data.userCart });
+  dispatch({ type: "SET_LOADING" });
+};
 
 const addToWishlist = async (event, productID, dispatch, isLoggedIn) => {
-    dispatch({ type: 'SET_LOADING' })
-    event.preventDefault()
-    if (isLoggedIn) {
-        try {
-            const { data } = await networkCall(`/wishlist/${productID}`, 'POST')
-            if (data.success) {
-                dispatch({ type: 'SET_WISHLIST', payload: data.response })
-                callToastify(' 💜 Added to wishlist')
-            } else {
-                callToastify(data.message)
-            }
-        } catch {
-            callToastify('Something went wrong ! Try Again')
-        }
+  dispatch({ type: "SET_LOADING" });
+  event.preventDefault();
+  if (isLoggedIn) {
+    try {
+      const { data } = await networkCall(`/wishlist/${productID}`, "POST");
+      if (data.success) {
+        dispatch({ type: "SET_WISHLIST", payload: data.response });
+        callToastify(" 💜 Added to wishlist");
+      } else {
+        callToastify(data.message);
+      }
+    } catch {
+      callToastify("Something went wrong ! Try Again");
     }
-    else {
-        callToastify('You are not logged in', 'Error')
-    }
+  } else {
+    callToastify("You are not logged in", "Error");
+  }
 
-    dispatch({ type: 'SET_LOADING' })
-}
-
+  dispatch({ type: "SET_LOADING" });
+};
 
 const getUserWishlist = async (dispatch) => {
-    dispatch({ type: 'SET_LOADING' })
-    try {
-        const { data } = await networkCall('/wishlist', 'GET')
-        dispatch({ type: 'SET_WISHLIST', payload: data.userWishlist })
-    } catch (err) {
-        console.log(`Error: ${err.message}`);
-    }
-    dispatch({ type: 'SET_LOADING' })
-}
+  dispatch({ type: "SET_LOADING" });
+  try {
+    const { data } = await networkCall("/wishlist", "GET");
+    dispatch({ type: "SET_WISHLIST", payload: data.userWishlist });
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+  }
+  dispatch({ type: "SET_LOADING" });
+};
 
-export { networkCall, defaultHeaderForToken, getUserCart, addToCart, addToWishlist, modifyCartItemsQty, removeItemFromCart, getUserWishlist }
+export {
+  networkCall,
+  defaultHeaderForToken,
+  getUserCart,
+  addToCart,
+  addToWishlist,
+  modifyCartItemsQty,
+  removeItemFromCart,
+  getUserWishlist,
+};
