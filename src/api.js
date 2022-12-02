5a9210300796a1cd81905e56603b0e6979a3d88d
Gokul Gopi <gokulg1412@gmail.com>
2022-12-02 09:40:34 +0530

    fix: added correct api

--- src/api.js
+++ src/api.js
@@ -1,2 +1,1 @@
-export const Backend = "https://superior-sounds-backend.herokuapp.com";
-// export const Backend = "http://localhost:5000";
+export const Backend = "https://superior-sounds-backend.onrender.com";
